e0b2bb5e20b091ddb59a7abfb3f0e378ba437c16
cindy0310 <cindy.dai@edu.devinci.fr>
2021-03-26 18:45:30 +0100

    TD5

---
+++ Projet-S4-TD2/Complexes.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projet_S4_TD2
+{
+    public class Complexes
+    {
+        public double a; // Partie Réelle
+        public double b; // Partie Imaginaire
+
+        public Complexes(double a, double b)
+        {
+            this.a = a;
+            this.b = b;
+        }
+
+        /// <summary>
+        /// Calcule le module |Z| = Sqrt(a*a + b*b).
+        /// </summary>
+        /// retourne le module d'un nombre complexe
+        public double Module()
+        {
+            return Math.Sqrt(a * a + b * b);
+        }
+        public void Add(Complexes c)
+        {
+            a = a + c.a;
+            b = b = c.b;
+        }
+    }
+}
---
+++ Projet-S4-TD2/Pixel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Projet_S4_TD2
+{
+    class Pixel
+    {
+    }
+}
